44ef4a5074f0f7f53e5ddab33a6a6137dadae142
Matt Crook <mattlcrook@icloud.com>
2023-03-17 00:02:03 +1300

    implement grpc json transcoding

--- src/service/HelloDapr.Service.Greeter/Program.cs
+++ src/service/HelloDapr.Service.Greeter/Program.cs
@@ -2,13 +2,31 @@ using HelloDapr.Service.Greeter.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(options =>
+{
+    var filePath = Path.Combine(AppContext.BaseDirectory, "HelloDapr.Contracts.xml");
+    options.IncludeXmlComments(filePath);
+    options.IncludeGrpcXmlComments(filePath, includeControllerXmlComments: true);
+});
+
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc().AddJsonTranscoding();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
